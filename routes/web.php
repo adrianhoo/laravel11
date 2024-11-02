7ccf4fd6425134849bc0bd19ba5b3c43f4993bbf
adrianhoo <andrianhowandi@gmail.com>
2024-11-02 23:55:01 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Adrian",
+        "img" => "adrian.jpg",
+        "email" => "andrianhowandi@gmail.com",
+    ]);
+});
+
+route::get('/home', function () {
+    return view("home");
 });
